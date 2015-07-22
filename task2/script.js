900dc3ac2fc183c236223084217775bfdbfc2517
kabalkin <bestworldme@mail.ru>
2015-07-22 17:31:49 +0300

    Add project

---
+++ task2/script.js
@@ -0,0 +1,15 @@
+﻿
+i = 0;
+j=0;
+function press1(f) {
+
+if(i % 2===0) f.but.value = "Стоп"
+else f.but.value = "Старт"
+i++
+}
+function press2(f) {
+
+if(j % 2===0) f.but.value = "Отключиться"
+else f.but.value = "Подключиться"
+j++
+}
